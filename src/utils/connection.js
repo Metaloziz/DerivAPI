1ce2ab0ec03b6feedc04160c1917bbd7403be5d5
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-10-01 12:00:00 +0300

    learning

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,6 @@
 import DerivAPI from "@deriv/deriv-api";
 
-const connection = new WebSocket('wss://frontend.binaryws.com/websockets/v3?l=EN&app_id=1234');
+// const connection = new WebSocket('wss://frontend.binaryws.com/websockets/v3?l=EN&app_id=1234');
 
-export const api = new DerivAPI({endpoint: 'ws.binaryws.com', app_id: 1234});
+
 
